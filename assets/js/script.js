2d3a79d199b3252ff29392d291ee420c7fed3b39
hackpres <102390515+hackpres@users.noreply.github.com>
2022-05-16 23:01:51 -0400

    added and styled the timer. Still need to implement timer reduction on incorrect answer.

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,9 +5,11 @@ const restartBtn = document.getElementById("quiz-restart-btn");
 const titleContainer = document.getElementById("title-container");
 const questionContainer = document.getElementById("quiz-container");
 const completeContainer = document.getElementById("quiz-complete-container");
+const timerContainer = document.getElementById("timer-container")
 const questionElement = document.getElementById("question");
 const answerBtnElements = document.getElementById("answer-btn-container");
 
+var time = document.getElementById("timer");
 let shuffledQuestions, currentQuestionIndex;
 
 startBtn.addEventListener("click", startQuiz);
@@ -27,17 +29,51 @@ function startQuiz() {
     currentQuestionIndex = 0;
     questionContainer.classList.remove("hide");
     nextQuestion();
+    timer(time, 2, 15);
 }
+
+
+function timer(element, minutes, seconds) {
+    var timeLeft = minutes * 60 + seconds;
+    timerContainer.classList.remove("hide");
+    var interval = setInterval(function() {
+        
+        if (timeLeft == 0) {
+            setTimeout(function()  {
+                element.innerHTML = "time is up"
+            }, 10);
+
+            clearInterval(interval);
+
+            setTimeout(function() {
+            questionContainer.classList.add("hide");
+            completeContainer.classList.remove("hide");
+            }, 1500);
+
+            setTimeout(function() {
+                timerContainer.classList.add("hide");
+            }, 7000);
+        }
+        var minutes = Math.floor(timeLeft / 60);
+        if (minutes < 10) minutes = "0" + minutes;
+        var seconds = timeLeft % 60;
+        if (seconds < 10) seconds = "0" + seconds;
+        var text = `${minutes}:${seconds}`;
+        element.innerHTML = text;
+        timeLeft--;
+    }, 1000);
+    return timeLeft
+}
+
 // when the correct answer is chosen a green animation happens on the button and then the next question loads
 // when the incorrect answer is chosen a red animation happens on the button, the next question loads, and 10 seconds are removed from the timer.
 function nextQuestion() {
-    showQuestion(shuffledQuestions[currentQuestionIndex])
-    
-
+    showQuestion(shuffledQuestions[currentQuestionIndex]);
 }
 
 function showQuestion(question) {
     questionElement.innerText = question.question
+    resetState();
     question.answers.forEach(answer => {
         const button = document.createElement("button");
         button.innerText = answer.text;
@@ -56,8 +92,8 @@ function resetState() {
     }
 }
 
-function selectAnswer(e) {
-    const selectedBtn = e.target;
+function selectAnswer(event) {
+    const selectedBtn = event.target;
     const correct = selectedBtn.dataset.correct;
     Array.from(answerBtnElements.children).forEach(button => {
         setStatusClass(button, button.dataset.correct);
@@ -66,7 +102,7 @@ function selectAnswer(e) {
         nextBtn.classList.remove("hide");
     } else {
         setTimeout(function() {
-           questionContainer.classList.add("hide");
+            questionContainer.classList.add("hide");
             completeContainer.classList.remove("hide"); 
         }, 1500);
     }
@@ -96,20 +132,20 @@ const questions = [
             {text: "lastIndexOf()", correct: false},
             {text: "length()", correct: true}
         ]},
-    {question: "",
+    {question: "The Math method random() returns what?",
         answers: [
-            {text: "", correct: false},
-            {text: "", correct: false},
-            {text: "", correct: false},
-            {text: "", correct: false},
+            {text: "a number between 0 and 1", correct: true},
+            {text: "a number rounded down", correct: false},
+            {text: "a number between 1 - 10", correct: false},
+            {text: "a boolean", correct: false},
         ]},
-    // {question: "",
-    //     answers: [
-    //         {text: "", correct: false},
-    //         {text: "", correct: false},
-    //         {text: "", correct: false},
-    //         {text: "", correct: false},
-    //     ]},
+    {question: "What does isNaN() do?",
+        answers: [
+            {text: "parses a string and returns an integer", correct: false},
+            {text: "converts an object's value to a number", correct: false},
+            {text: "determines whether a value is an illegal number", correct: true},
+            {text: "returns the primitive value of a boolean", correct: false},
+        ]},
     // {question: "",
     //     answers: [
     //         {text: "", correct: false},
